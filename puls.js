a1d2b1e67ed41176887f663c61d70703066d60a8
fightagainstchinazi <fightagainstchinazri@liberatehongkong.com>
2022-12-13 23:33:12 +0800

    querySekl

---
+++ puls.js
@@ -0,0 +1,9 @@
+var elem = document.querySelector('img');
+var animation = elem.animate({
+  opacity: [0.5, 1],
+  transform: ['scale(0.5)', 'scale(1)'],
+}, {
+  direction: 'alternate',
+  duration: 500,
+  iterations: Infinity,
+});
