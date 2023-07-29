15386d990f8d17be7e3783c9de9e0854274418c1
tvpApache <killchinazi@chinazi.kl>
2023-07-29 09:22:31 +0800

    changeview

--- puls.js
+++ puls.js
@@ -19,7 +19,7 @@ var aliceTiming = {
   iterations: Infinity
 }
 
-document.querySelector("img").animate(
-  aliceTumbling, 
-  aliceTiming
-)
+//document.querySelector("img").animate(
+//  aliceTumbling, 
+//  aliceTiming
+//)
